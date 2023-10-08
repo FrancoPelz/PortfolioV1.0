f27ddd031b47472072af9bb59093bd6f733c0ea4
FrancoPelz <pelaezmontt.franco@gmail.com>
2023-10-07 22:16:06 -0300

    layout config basic

--- portfoliov1.0/src/App.jsx
+++ portfoliov1.0/src/App.jsx
@@ -3,9 +3,12 @@ import './css/app.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+      <div class="content">
+        <header class="header">Header</header>
+        <article class="main">Main</article>
+        <aside class="aside">Aside</aside>
+        <footer class="footer">Footer</footer>
+    </div>
   )
 }
 
